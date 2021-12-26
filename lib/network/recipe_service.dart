33a5d8ad22b083db13642c26b432f60d864be727
Erick Njoroge <erick.njoroge@betika.com>
2021-12-26 23:22:19 +0300

    wire up inteterceptors to decorate requests and converters to transform responses into model classes in API service

--- lib/network/recipe_service.dart
+++ lib/network/recipe_service.dart
@@ -3,6 +3,8 @@ import 'recipe_model.dart';
 import 'model_converter.dart';
 import 'model_response.dart';
 
+part 'recipe_service.chopper.dart';
+
 const String apiKey = '48bc65493e651396d3e02799a8c9bda6';
 const String apiId = '79e45cca';
 const String apiUrl = 'https://api.edamam.com/search';
@@ -13,25 +15,27 @@ abstract class RecipeService extends ChopperService {
   Future<Response<Result<APIRecipeQuery>>> queryRecipes(
       @Query('q') String query, @Query('from') int from, @Query('to') int to);
   //TODO: Add create
+  static RecipeService create() {
+    final client = ChopperClient(
+      baseUrl: apiUrl,
+      interceptors: [_addQuery, HttpLoggingInterceptor()],
+      converter: ModelConverter(),
+      errorConverter: const JsonConverter(),
+      services: [_$RecipeService()],
+    );
+
+    return _$RecipeService(client);
+  }
 }
 
 //TODO: Add _addQuery
+Request _addQuery(Request req) {
+  final params = Map<String, dynamic>.from(req.parameters);
 
-// class RecipeService {
-//   Future getData(String url) async {
-//     print('Calling url: $url');
-//     final response = await get(Uri.parse(url));
-//     if (response.statusCode == 200) {
-//       return response.body;
-//     } else {
-//       print(response.statusCode);
-//     }
-//   }
+  //Add apiId and apiKey to request parameters
+  params['api_id'] = apiId;
+  params['api_key'] = apiKey;
 
-//   Future<dynamic> getRecipes(String query, int from, int to) async {
-//     final recipeData = getData(
-//         '$apiUrl?app_id=$apiId&app_key=$apiKey&q=$query&from=$from&to=$to');
-
-//     return recipeData;
-//   }
-// }
+  //Return new copy of the request with the params
+  return req.copyWith(parameters: params);
+}
