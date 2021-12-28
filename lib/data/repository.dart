dc4ec4d9b0d9dbeb5381f5d7a88474183cc3f5ec
Erick Njoroge <erick.njoroge@betika.com>
2021-12-29 01:38:41 +0300

    create repository interface

---
+++ lib/data/repository.dart
@@ -0,0 +1,32 @@
+import 'models/models.dart';
+
+abstract class Repository {
+  //TODO: Add find methods
+  List<Recipe> findAllRecipes();
+
+  Recipe findRecipeById(int id);
+
+  List<Ingredient> findAllIngredients();
+
+  List<Ingredient> findRecipeIngredients(int recipeId);
+
+  //TODO: Add insert methods
+  int insertRecipe(Recipe recipe);
+
+  List<int> insertIngredients(List<Ingredient> ingredients);
+
+  //TODO: Add delete methods
+  void deleteRecipe(Recipe recipe);
+
+  void deleteIngredient(Ingredient ingredient);
+
+  void deleteIngredients(List<Ingredient> ingredients);
+
+  void deleteRecipeIngredients(int recipeId);
+
+  //TODO: Add intializing and closing methods
+  // Allow repository to initiliaze - e.g. DB start
+  Future init();
+
+  void close();
+}
