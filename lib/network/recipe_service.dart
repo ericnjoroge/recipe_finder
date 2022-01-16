95543d21b5c2b3e56d2df5d61592491471680b08
Erick Njoroge <erick.njoroge@betika.com>
2022-01-16 22:00:58 +0300

    implement service interface to allow switching between recipeService and mockService

--- lib/network/recipe_service.dart
+++ lib/network/recipe_service.dart
@@ -2,6 +2,7 @@ import 'package:chopper/chopper.dart';
 import 'recipe_model.dart';
 import 'model_converter.dart';
 import 'model_response.dart';
+import 'service_interface.dart';
 
 part 'recipe_service.chopper.dart';
 
@@ -10,7 +11,8 @@ const String apiId = '79e45cca';
 const String apiUrl = 'https://api.edamam.com';
 
 @ChopperApi()
-abstract class RecipeService extends ChopperService {
+abstract class RecipeService extends ChopperService implements ServiceInterface {
+  @override
   @Get(path: 'search')
   Future<Response<Result<APIRecipeQuery>>> queryRecipes(
       @Query('q') String query, @Query('from') int from, @Query('to') int to);
