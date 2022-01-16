77f29f2ec28b9e32e6ea245d3e85dbfb610f7303
Erick Njoroge <erick.njoroge@betika.com>
2022-01-16 17:24:29 +0300

    remove streams code

--- lib/data/repository.dart
+++ lib/data/repository.dart
@@ -2,31 +2,27 @@ import 'models/models.dart';
 
 abstract class Repository {
   //TODO: Add find methods
-  Future<List<Recipe>> findAllRecipes();
+  List<Recipe> findAllRecipes();
 
-  Stream<List<Recipe>> watchAllRecipes();
+  Recipe findRecipeById(int id);
 
-  Future<Recipe> findRecipeById(int id);
+  List<Ingredient> findAllIngredients();
 
-  Future<List<Ingredient>> findAllIngredients();
-
-  Stream<List<Ingredient>> watchAllIngredients();
-
-  Future<List<Ingredient>> findRecipeIngredients(int recipeId);
+  List<Ingredient> findRecipeIngredients(int recipeId);
 
   //TODO: Add insert methods
-  Future<int> insertRecipe(Recipe recipe);
+  int insertRecipe(Recipe recipe);
 
-  Future<List<int>> insertIngredients(List<Ingredient> ingredients);
+  List<int> insertIngredients(List<Ingredient> ingredients);
 
   //TODO: Add delete methods
-  Future<void> deleteRecipe(Recipe recipe);
+  void deleteRecipe(Recipe recipe);
 
-  Future<void> deleteIngredient(Ingredient ingredient);
+  void deleteIngredient(Ingredient ingredient);
 
-  Future<void> deleteIngredients(List<Ingredient> ingredients);
+  void deleteIngredients(List<Ingredient> ingredients);
 
-  Future<void> deleteRecipeIngredients(int recipeId);
+  void deleteRecipeIngredients(int recipeId);
 
   //TODO: Add intializing and closing methods
   // Allow repository to initiliaze - e.g. DB start
